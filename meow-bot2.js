17d1256986579d21e6e965a6f7ac66b0fd217cdd
nicoseddio <nico.seddio@live.com>
2021-01-10 12:33:31 -0500

    migrated Kernel to System, added helper functions

--- meow-bot2.js
+++ meow-bot2.js
@@ -3,21 +3,21 @@ const auth = require('./auth.json');
 
     const fs = require('fs');
     const configFileName = './config.json'; // for config file saving
-    const cfg = require(configFileName);
-    const clientVersion = 2.000;
-    const prefixes = ['!'];
+    const config = require(configFileName);
+    const System = require('./lib/System');
 
 const client = new Discord.Client();
+const sys = new System(client,config);
 client.login(auth.token);
 
 client.on('ready', () => {
     console.log();
     console.log(`Logged in as ${client.user.tag}!`);
-
-    prefixes.push(`<@!${client.user.id}>`);
-    // test()
-    let stuff = loadApps('./lib/apps/',cfg);
-    console.log(cfg.Apps);
+    
+    sys.initialize();
+    let stuff = loadApps('./lib/apps/');
+    console.log(stuff);
+    console.log(sys.generateToken(auth.token));
 });
 
 
@@ -39,69 +39,14 @@ client.on('messageDelete', async function(message) {
     
 });
 
-function loadApps(dir='./lib/apps/',config={},appExt='.app') {
+
+function loadApps(dir='./lib/apps/',appExt='.app') {
     const apps = {};
     fs.readdirSync(dir).forEach(function (appName) {
         if (appName.endsWith(appExt)) {
             const app = require(dir+appName);
             apps[appName.replace(appExt,'')] = new app;
-        }
-      
-        // __defineGetter__ is a getter method which will be called if particluar
-        // property (or submodule in our case) will be requested
-        // exports.__defineGetter__(appName, function () {
-      
-        //   return require('./submodules/' + appName);
-      
-        // });
-      
-      });
-      config.Apps = apps;
-    // const appExt = '.app';
-    // const apps = {};
-    // const events = {};
-    // let files = [];
-    // try {
-    //     files = fs.readdirSync(dir);
-    // } catch (error) {
-    //     throw new Error(`${dir} is not a valid directory.`);
-    // }
-
-    // for (f in files) {
-    //     let appFileName = files[f];
-    //     if (appFileName.endsWith(appExt)) {
-    //         let appName = 
-    //             appFileName.substring(
-    //                 0,
-    //                 appFileName.length-appExt.length
-    //             );
-    //         let appFileNamePath = `${dir}${appFileName}/`;
-    //         const app = require(appFileNamePath);
-    //         apps[appName] = new app;
-
-    //         let reqs = apps[appName].listeners();
-
-    //         events[appName] = reqs.events;
-
-    //         for (c in reqs.commands) {
-    //             let a = reqs.commands[c];
-    //             if (!(a in cmdAliases)) {
-    //                 cmdAliases[a] = appName;
-    //             } else {
-    //                 cmdAliases[`${appName}-${a}`] =
-    //                     appName;
-    //             }
-    //         }
-    //     }
-    // }
-
-    // // clean up aliases
-    // for (a in cmdAliases) {
-    //     let appFileName = cmdAliases[a] + appExt;
-    //     if (!(files.includes(appFileName))) {
-    //         delete cmdAliases[a];
-    //     }
-    // }
-
-    // return {apps, cmdAliases, events};
+        }      
+    });
+    return apps;
 }
