17c4c8078ecb2164c7417811a61622b0c15dc892
nicoseddio <nico.seddio@live.com>
2021-02-02 22:42:53 -0500

    moved kernel to /lib

---
+++ meow-bot3.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const auth = require('./auth.json');
+
+const Kernel = require('./lib/Kernel');
+const kernel = new Kernel();
+
+const client = new Discord.Client();
+client.login(auth.token);
+
+client.on('ready', () => {
+    console.log();
+    console.log(`Logged in as ${client.user.tag}!`);
+
+    kernel.initialize(client);
+});
+client.on('message', async function(message) {
+    if (message.author.id === client.user.id) return; //ignore self
+    kernel.distributeEvent('message', message);
+    kernel.distribute(message);
+});
+client.on('messageDelete', async function(message) {
+    kernel.distributeEvent('messageDelete', message);
+});
